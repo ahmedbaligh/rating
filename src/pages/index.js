5158202d84b8e554599c59d70e93aafd171b5d5c
Ahmed Baligh <58535713+ahmedbaligh@users.noreply.github.com>
2021-07-17 06:06:12 +0200

    feat: Implement fully responsive signin page
    Add login functionality with error messages and redirection.
    Implement full responsive on all screen size.
    Add full support for Arabic language.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 export { default as Home } from './Home/Home';
 export { default as Product } from './Product/Product';
 export { default as Signup } from './Signup/Signup';
+export { default as Signin } from './Signin/Signin';
 export { default as Error404 } from './Error404/Error404';
 export { default as Scraper } from './Scraper/Scraper';
 export { default as Admin } from './Admin/Admin';
