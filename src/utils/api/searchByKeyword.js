4dfb90f22281baa36072a50896db38ba7ddf2437
mohamedagina <mohamedagina001@gmail.com>
2021-07-18 09:50:08 +0200

    style: make one function for search

--- src/utils/api/searchByKeyword.js
+++
@@ -1,8 +0,0 @@
-import axiosDefault from './api';
-const searchByKeyword = ({ keyword, page = 0, maxCount = 20 }) =>
-  axiosDefault({
-    url: `/api/services/app/Product/GetProductsByKeyword/?KeyWord=${keyword}&SkipCount=${
-      maxCount * page
-    }&MaxCount=${maxCount}`
-  });
-export default searchByKeyword;
