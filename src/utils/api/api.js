129f7338a3721fa0cbe2bd04a6e5f80033e12ee8
Ahmed Baligh <58535713+ahmedbaligh@users.noreply.github.com>
2021-07-18 04:29:32 +0200

    chore: Update API headers

--- src/utils/api/api.js
+++ src/utils/api/api.js
@@ -6,7 +6,14 @@ import { toggleLoading } from '../../redux/actions/loading';
 const axiosDefault = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
   headers: {
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json; charset=utf-8',
+    'Content-Encoding': 'gzip',
+    Vary: 'Accept - Encoding',
+    Server: 'Microsoft - IIS / 10.0',
+    'X-Content-Type-Options': 'nosniff',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'X-Powered-By': 'ASP.NET'
   }
 });
 
